108cc7f702e7af3caf10220423d6d60fb3bd104b
Francois Posseik <f.posseik@fz-juelich.de>
2020-01-28 15:57:33 +0000

    include explicitely the module in setup.py as py_modules

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Torricelli-fposseik_tmp", # Replace with your own username
-    version="1.0rc0",
+    name="Torricelli-fposseik", # Replace with your own username
+    version="3.0rc0",
     author="Francois C. Bocquet",
     author_email="f.posseik@fz-juelich.de",
     description="A software to determine atomic spatial distribution from normal incidence x-ray standing wave data",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/fposseik/Torricelli",
     packages=setuptools.find_packages(),
+    py_modules=['Torricelli'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
