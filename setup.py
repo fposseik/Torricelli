ed599a666a9ac8b15b8053dec9e27d8eeb96f4c2
Francois Posseik <f.posseik@fz-juelich.de>
2020-02-06 15:54:15 +0100

    Go to version number 4.0.2 consistently in setup.py, torricelli.py and the manual.tex. This is not on pypi yet.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torricelli",
-    version="4.0.1",
+    version="4.0.2",
     author="Francois C. Bocquet",
     author_email="f.posseik@fz-juelich.de",
     long_description=long_description,
