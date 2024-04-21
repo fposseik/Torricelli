cfe69e69641fb582eecc1a1c071f7f6a07b3db1e
Francois posseik <f.posseik@fz-juelich.de>
2024-04-21 20:23:01 +0200

    Issue of jumping phase solved for most cases.
    The +pi in Eq. 74+75 of the Torricelli paper was applied only to the minus branch. Now also to the plus branch.

--- Torricelli/torricelli.py
+++ Torricelli/torricelli.py
@@ -1104,39 +1104,40 @@ class Torricelli(QMainWindow):
               / ( np.absolute(P_DCM)*gamma_mo*np.sqrt(np.absolute(b_mo)*F_H_DCM*F_Hbar_DCM) )
 
             ## Sample crystal:
-            EH_over_EO_plus_cr = -1 * (P / np.abs(P)) * (
-                eta_cr + np.sqrt(eta_cr**2 - 1)) * np.sqrt(
-                    np.absolute(self.b_cr) * F_H / F_Hbar)
-            EH_over_EO_minus_cr = -1 * (P / np.abs(P)) * (
-                eta_cr - np.sqrt(eta_cr**2 - 1)) * np.sqrt(
-                    np.absolute(self.b_cr) * F_H / F_Hbar)
-            phi_plus_cr = np.arctan(
-                np.imag(EH_over_EO_plus_cr) / np.real(EH_over_EO_plus_cr))
-            phi_minus_cr = np.arctan(
-                np.imag(EH_over_EO_minus_cr) / np.real(EH_over_EO_minus_cr))
-            refl_plus_cr = np.abs(EH_over_EO_plus_cr)**2
+            EH_over_EO_plus_cr  = -1 * (P / np.abs(P)) * ( eta_cr + np.sqrt(eta_cr**2 - 1)) * np.sqrt( np.absolute(self.b_cr) * F_H / F_Hbar)
+            EH_over_EO_minus_cr = -1 * (P / np.abs(P)) * ( eta_cr - np.sqrt(eta_cr**2 - 1)) * np.sqrt( np.absolute(self.b_cr) * F_H / F_Hbar)
+            phi_plus_cr  = np.arctan( np.imag(EH_over_EO_plus_cr ) / np.real(EH_over_EO_plus_cr ))
+            phi_minus_cr = np.arctan( np.imag(EH_over_EO_minus_cr) / np.real(EH_over_EO_minus_cr))
+            refl_plus_cr  = np.abs(EH_over_EO_plus_cr )**2
             refl_minus_cr = np.abs(EH_over_EO_minus_cr)**2
 
             #print np.real(EH_over_EO_minus_cr)
             if refl_plus_cr > 0 and refl_plus_cr < 1:  # choose the branch that has a physically meaningful reflectivity.
                 self.Theory_Refl_sample[i] = refl_plus_cr
                 self.Theory_Phase_Sample[i] = phi_plus_cr
-                #print("ph.E: {:6.2f} | *refl_plus_cr: {:8.2f} | refl_minus_cr: {:8.2f} | *phi_plus_cr: {:8.2f} | phi_minus_cr: {:8.2f} ".format(self.Theory_photonEnergy[i], refl_plus_cr,refl_minus_cr,phi_plus_cr,phi_minus_cr))
+                #print("PLU: ph.E: {:6.2f} | *refl_plus_cr: {:8.2f} | refl_minus_cr: {:8.2f} | *phi_plus_cr: {:8.2f} | phi_minus_cr: {:8.2f} ".format(self.Theory_photonEnergy[i], refl_plus_cr,refl_minus_cr,phi_plus_cr,phi_minus_cr))
+
+                ### NEW
+                if np.real(EH_over_EO_plus_cr) < 0 and self.ui.radioButton_sigma_pol_light.isChecked():  # from Zegenhagen 1993 Eq. 2.10, 2.11
+                    self.Theory_Phase_Sample[i] += np.pi
+                # Taking change of sign in real(EH/E0) into account due to the change of sign in the polarization factor when using pi-polarization
+                elif np.real(EH_over_EO_plus_cr) > 0 and self.ui.radioButton_pi_pol_light.isChecked():
+                    self.Theory_Phase_Sample[i] += np.pi
+                    #print("PLU: ph.E: {:6.2f} | refl_plus_cr: {:8.2f} | *refl_minus_cr: {:8.2f} | phi_plus_cr: {:8.2f} | *phi_minus_cr: {:8.2f} ".format(self.Theory_photonEnergy[i], refl_plus_cr, refl_minus_cr, phi_plus_cr + np.pi, phi_minus_cr + np.pi))
+                ### end NEW
+
+                #print("PLU: ph.E: {:6.2f} | *refl_plus_cr: {:8.2f} | refl_minus_cr: {:8.2f} | *phi_plus_cr: {:8.2f} | phi_minus_cr: {:8.2f} ".format(self.Theory_photonEnergy[i], refl_plus_cr,refl_minus_cr,phi_plus_cr,phi_minus_cr))
+                
             else:
                 self.Theory_Refl_sample[i] = refl_minus_cr
                 self.Theory_Phase_Sample[i] = phi_minus_cr
 
-                if np.real(
-                        EH_over_EO_minus_cr
-                ) < 0 and self.ui.radioButton_sigma_pol_light.isChecked(
-                ):  # from Zegenhagen 1993 Eq. 2.10, 2.11
+                if np.real(EH_over_EO_minus_cr) < 0 and self.ui.radioButton_sigma_pol_light.isChecked():  # from Zegenhagen 1993 Eq. 2.10, 2.11
                     self.Theory_Phase_Sample[i] += np.pi
                 # Taking change of sign in real(EH/E0) into account due to the change of sign in the polarization factor when using pi-polarization
-                elif np.real(
-                        EH_over_EO_minus_cr
-                ) > 0 and self.ui.radioButton_pi_pol_light.isChecked():
+                elif np.real(EH_over_EO_minus_cr) > 0 and self.ui.radioButton_pi_pol_light.isChecked():
                     self.Theory_Phase_Sample[i] += np.pi
-                    #print("ph.E: {:6.2f} | refl_plus_cr: {:8.2f} | *refl_minus_cr: {:8.2f} | phi_plus_cr: {:8.2f} | *phi_minus_cr: {:8.2f} ".format(self.Theory_photonEnergy[i], refl_plus_cr, refl_minus_cr, phi_plus_cr + np.pi, phi_minus_cr + np.pi))
+                    #print("MIN: ph.E: {:6.2f} | refl_plus_cr: {:8.2f} | *refl_minus_cr: {:8.2f} | phi_plus_cr: {:8.2f} | *phi_minus_cr: {:8.2f} ".format(self.Theory_photonEnergy[i], refl_plus_cr, refl_minus_cr, phi_plus_cr + np.pi, phi_minus_cr + np.pi))
                 else:
                     pass
                     #print("ph.E: {:6.2f} | refl_plus_cr: {:8.2f} | *refl_minus_cr: {:8.2f} | phi_plus_cr: {:8.2f} | *phi_minus_cr: {:8.2f} ".format(self.Theory_photonEnergy[i], refl_plus_cr, refl_minus_cr, phi_plus_cr, phi_minus_cr))
